9444ea2270239f9b659cfaf6dfe0a3d052dabf4e
xiangw <xiang092@gmail.com>
2018-11-27 11:19:07 +0800

    modify first

--- src/include_path.cpp
+++
@@ -1,9 +0,0 @@
-#include <opencv2/core/version.hpp>
-// #include "opencv2/core.hpp"
-#include <iostream>
-
-int main()
-{
-	std::cout << "hello" << std::endl;
-	return 0;
-}
--- src/mat_data.py
+++
@@ -1,5 +0,0 @@
-import cv2
-from PIL import Image, ExifTags
-
-cvimage = cv2.imread('1.jpg')
-image = Image.fromarray(cvimage)
